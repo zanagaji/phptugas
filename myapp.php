38afb33f8b2c53ae6a79143667ad96ff251fae9b
sangaji <79409143+zanagaji@users.noreply.github.com>
2022-09-29 22:26:14 +0700

    Add files via upload

---
+++ myapp.php
@@ -0,0 +1,20 @@
+<?php
+echo "<br> menggunakan perintah echo";
+echo "<br> selamat datang di matkul praktikum framework";
+print "menggunakan perintah print";
+print "selamat datang di matkul praktikum framework";
+
+$nama= "the prediksi";
+$no= 4;
+$desimal= 0.5;
+
+echo "pengguna echo";
+echo "nama kelompok ".$nama."<br>";
+echo "nomor ".$no."<br>";
+echo "desimal ".$desimal."<br>";
+
+print "pengguna print";
+print "nama kelompok ".$nama."<br>";
+print "no ".$no."<br>";
+print "desimal ".$desimal."<br>";
+?>
